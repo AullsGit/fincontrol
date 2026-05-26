3f74beeb38b64da7ffbef182fc60a8d1ba65554e
shirlycresia-cloud <shirlycresia@gmail.com>
2026-05-26 19:04:03 +0200

    Update app_constants.dart

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,20 +1,20 @@
 class AppConstants {
-  // Firebase Collections
+  
   static const String usersCollection = 'utilizadores';
   static const String gastosCollection = 'gastos';
   static const String metasCollection = 'metas';
   static const String dicasCollection = 'dicas';
 
-  // Hive Boxes
+  
   static const String gastosBox = 'gastos_box';
   static const String metasBox = 'metas_box';
   static const String pendingBox = 'pending_sync_box';
 
-  // Currency
+  
   static const String currency = 'MZN';
   static const String currencySymbol = 'MT';
 
-  // Categorias de Despesas
+  
   static const List<String> categoriasDespesas = [
     'Mercearia',
     'Renda',
@@ -37,7 +37,7 @@ class AppConstants {
     'Outros',
   ];
 
-  // Category Icons mapping
+  
   static const Map<String, String> categoryEmojis = {
     'Mercearia': '🛒',
     'Renda': '🏠',
